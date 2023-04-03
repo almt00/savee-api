91ecdcec061fb48738f91992a6fe7ea838da1a02
amieUA <6574688+heyamie@users.noreply.github.com>
2023-04-03 18:23:17 +0100

    Corrigir consumption user

--- controllers/userController.js
+++ controllers/userController.js
@@ -63,17 +63,36 @@ router.delete("/:user_id", (req, res) => {
 });
 
 router.get("/:user_id/consumption", function (req, res) {
-  res.send({
-    user_id: `${req.params.user_id}`,
-    consumption_id: 1,
-    house_id: 1,
-    payment_id: 1,
-    routine_id: 1,
-    task_id: 1,
-    consumption_date: "2022-12-01",
-    consumption: 134,
-    type: 2,
-  });
+  res.send([
+    {
+      task_id: 1,
+      start_time: "2022-12-01T00:00:00.000Z",
+      end_time: "2022-12-01T00:30:00.000Z",
+      duration: "30",
+      ref_task_id: 2,
+    },
+    {
+      task_id: 2,
+      start_time: "2022-12-01T00:00:00.000Z",
+      end_time: "2022-12-01T00:15:00.000Z",
+      duration: "15",
+      ref_task_id: 3,
+    },
+    {
+      task_id: 3,
+      start_time: "2022-12-01T00:00:00.000Z",
+      end_time: "2022-12-01T00:30:00.000Z",
+      duration: "25",
+      ref_task_id: 1,
+    },
+    {
+      task_id: 4,
+      start_time: "2022-12-01T00:00:00.000Z",
+      end_time: "2022-12-01T00:15:00.000Z",
+      duration: "40",
+      ref_task_id: 2,
+    },
+  ]);
 });
 
 router.get("/:user_id/task", function (req, res) {
