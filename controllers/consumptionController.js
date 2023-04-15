f9d4970a79f593c85d729936efd25da7f443b72b
Ana Luísa <86081728+almt00@users.noreply.github.com>
2023-04-15 19:48:58 +0100

    added consumption reqs to controller

--- controllers/consumptionController.js
+++ controllers/consumptionController.js
@@ -1,60 +1,25 @@
 const express = require("express");
+const prisma = require("../lib/prisma.js");
 const router = express.Router();
 
-router.get("(/user/:user_id)", function (req, res) {
-  res.send([
-    {
-      task_id: 1,
-      start_time: "2022-12-01T00:00:00.000Z",
-      end_time: "2022-12-01T00:30:00.000Z",
-      duration: "30",
-      ref_task_id: 2,
+router.get("(/user/:user_id)", async function (req, res) {
+  const { user_id } = req.params;
+  const user_consumptions = await prisma.consumptionHistory.findMany({
+    where: {
+      user_id: parseInt(user_id),
     },
-    {
-      task_id: 2,
-      start_time: "2022-12-01T00:00:00.000Z",
-      end_time: "2022-12-01T00:15:00.000Z",
-      duration: "15",
-      ref_task_id: 3,
-    },
-    {
-      task_id: 3,
-      start_time: "2022-12-01T00:00:00.000Z",
-      end_time: "2022-12-01T00:30:00.000Z",
-      duration: "25",
-      ref_task_id: 1,
-    },
-    {
-      task_id: 4,
-      start_time: "2022-12-01T00:00:00.000Z",
-      end_time: "2022-12-01T00:15:00.000Z",
-      duration: "40",
-      ref_task_id: 2,
-    },
-  ]);
+  });
+  res.json(user_consumptions);
 });
 
-router.get("/house/:house_id", (req, res) => {
-  res.send([
-    {
-      consumption_id: 1,
-      payment_id: 1,
-      routine_id: 1,
-      task_id: 1,
-      consumption_date: "2022-12-01",
-      consumption: 134,
-      type: 2,
+router.get("/house/:house_id",async (req, res) => {
+  const { house_id } = req.params;
+  const house_consumptions = await prisma.consumptionHistory.findMany({
+    where: {
+      house_id: parseInt(house_id),
     },
-    {
-      consumption_id: 2,
-      payment_id: 1,
-      routine_id: 1,
-      task_id: 1,
-      consumption_date: "2022-12-01",
-      consumption: 134,
-      type: 2,
-    },
-  ]);
+  });
+  res.json(house_consumptions);
 });
 
 module.exports = router;
