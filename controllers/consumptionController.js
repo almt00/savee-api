be6c13b166a05b16ec1ff142b02b64fa5d582cba
amieUA <6574688+heyamie@users.noreply.github.com>
2023-04-25 15:25:01 +0100

    Consumption today

--- controllers/consumptionController.js
+++ controllers/consumptionController.js
@@ -31,6 +31,35 @@ router.get("(/user/:user_id)", async function (req, res) {
   res.json(user_consumptions);
 });
 
+router.get("(user/:user_id/today)", async function (req, res) {
+  const { user_id } = req.params;
+  const today_consumptions = await prisma.consumptionHistory.findMany({
+    where: {
+      user_id: parseInt(user_id),
+      consumption_date: {
+        gte: new Date(new Date().setHours(0, 0, 0, 0)),
+      }
+    },
+    include: {
+      task: {
+        select: {
+          start_time: true,
+          end_time: true,
+          task: true,
+        },
+      },
+      routine: {
+        select: {
+          duration_routine: true,
+          creation_routine: true,
+          task: true,
+        },
+      },
+    },
+  });
+  res.json(today_consumptions);
+});
+
 router.get("/house/:house_id", async (req, res) => {
   const { house_id } = req.params;
   const house_consumptions = await prisma.consumptionHistory.findMany({
