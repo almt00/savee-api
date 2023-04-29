09cf89fece4e1b4742fb82b3bf9aac5194b8dd84
amieUA <6574688+heyamie@users.noreply.github.com>
2023-04-29 17:41:35 +0100

    socorro

--- controllers/insightsController.js
+++ controllers/insightsController.js
@@ -1,36 +1,31 @@
 const express = require("express");
 const prisma = require("../lib/prisma.js");
 const router = express.Router();
+const consumptionController = require("./consumptionController.js");
 
 // insight by user
+// group consumptions by payment_id based on date_payment
 
 router.get("(/user/:user_id)", async function (req, res) {
-    const { user_id } = req.params;
-    const user_insights = await prisma.consumptionHistory.findMany({
-        where: {
-            user_id: parseInt(user_id),
+  const { user_id } = req.params;
+  const { user_consumption } = await consumptionController.get(
+    "/user/:user_id"
+  );
+
+  const user_insights = await prisma.userPayment.findMany({
+    where: {
+      user_id: parseInt(user_id),
+    },
+    include: {
+      payment: {
+        select: {
+          date_payment: true,
+          value_payment: true,
         },
-        orderBy: {
-            consumption_date: "desc",
-        },
-        include: {
-            task: {
-                select: {
-                    start_time: true,
-                    end_time: true,
-                    task: true,
-                },
-            },
-            routine: {
-                select: {
-                    duration_routine: true,
-                    creation_routine: true,
-                    task: true,
-                },
-            },
-        },
-    });
-    res.json(user_insights);
+      },
+    },
+  });
+  res.json(user_insights);
 });
 
 module.exports = router;
