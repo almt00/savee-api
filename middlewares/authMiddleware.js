d49e4c30730c1d99697258d17cb75b7a6e7f6426
amieUA <6574688+heyamie@users.noreply.github.com>
2023-05-11 09:37:29 +0100

    Adicionar middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,54 @@
+const jwt = require("jsonwebtoken");
+const { UserModel } = require("../schema.prisma"); // não sei se é assim
+const { error } = require("../utils/apiResponse");
+
+module.exports = async function authMiddleware(req, res, next) {
+  let token;
+
+  // Retirar o token do header do request ou das cookies
+  if (req.headers.authorization) {
+    token =
+      req.headers.authorization &&
+      req.headers.authorization.split(" ")[0] === "Bearer" &&
+      req.headers.authorization.split(" ")[1];
+  } else {
+    token = req.cookies.authorization;
+  }
+
+  if (!token) {
+    res.status(401).send(error("Unauthorized", 401));
+    return;
+  }
+
+  try {
+    // validação se o token é valido
+    const isValid = jwt.verify(token, process.env.TOKEN_SECRET);
+    if (!isValid) {
+      res.status(401).send(error("Unauthorized", 401));
+      return;
+    }
+
+    // se for válido extraímos a data
+    const { sub: userId, exp } = await jwt.decode(token);
+
+    if (exp <= Math.floor(Date.now() / 1000)) {
+      res.status(401).send(error("Unauthorized", 401));
+      return;
+    }
+
+    const user = await UserModel.findByPk(userId, {
+      include: [RoleModel],
+      attributes: { exclude: ["RoleId", "password"] },
+    });
+
+    if (!user) {
+      res.status(401).send(error("Unauthorized", 401));
+      return;
+    }
+
+    req.user = user;
+    next();
+  } catch (e) {
+    res.status(401).send(error("Unauthorized", 401));
+  }
+};
